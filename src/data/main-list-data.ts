033a43b2a0d84b84b24e952c7d23fdfd97534d1c
mohamed affes <med123011@gmail.com>
2025-03-15 21:08:08 +0000

    Update blog links to include trailing slashes and adjust profile picture dimensions

--- src/data/main-list-data.ts
+++ src/data/main-list-data.ts
@@ -2,23 +2,23 @@ import type List from '../model/main-list-model';
 
 const data: List[] = [
     {
-        to: '/blogs/database-management',
+        to: '/blogs/database-management/',
         title: 'The Importance of Efficient Database Management'
     },
     {
-        to: '/blogs/typescript-new-javascript',
+        to: '/blogs/typescript-new-javascript/',
         title: 'Typescript is the new Javascript'
     },
     {
-        to: '/blogs/c++-vs-java',
+        to: '/blogs/c++-vs-java/',
         title: 'C++ vs Java: Choosing the Right Language for Your Needs'
     },
     {
-        to: '/blogs/iot',
+        to: '/blogs/iot/',
         title: 'The Role of IoT in Modern Technology'
     },
     {
-        to: '/blogs/robotics',
+        to: '/blogs/robotics/',
         title: 'Robotics: The Next Frontier in Automation'
     }
 ]

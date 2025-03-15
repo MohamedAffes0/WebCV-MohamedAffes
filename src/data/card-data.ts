e977b5d78e2052869fd0b4701fcd7e01e23fd3f2
mohamed affes <med123011@gmail.com>
2025-03-15 11:39:28 +0000

    Add Netlify adapter, update image handling, and refine theme toggle functionality

--- src/data/card-data.ts
+++ src/data/card-data.ts
@@ -2,19 +2,19 @@ import type Card from '../model/card-model';
 
 const data: Card[] = [
     {
-        to: "/philosophies",
+        to: "/philosophies/",
         title: "Here are my guiding philosophies for life",
         gradient1: "#828CF8",
         gradient2: "#D7B4FE",
     },
     {
-        to: "/work-summary",
+        to: "/work-summary/",
         title: "A summary of my work history",
         gradient1: "#FDE68A",
         gradient2: "#FCA5A5",
     },
     {
-        to: "/beyond-tech",
+        to: "/beyond-tech/",
         title: "What I do beyond technology",
         gradient1: "#6EE4B8",
         gradient2: "#9335EA",
